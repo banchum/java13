be53b820e6067864813deb88611fce96fd0b5ef7
kbs <krttj1212@gmail.com>
2019-05-28 17:21:33 +0900

    commit

--- src/iducs/java/b/Animals.java
+++ src/iducs/java/b/Animals.java
@@ -1,7 +1,11 @@
 package iducs.java.b;
 
 public interface Animals {
-	void move();
+	//public final double Height=172.3;
+	public final String species = "동물";
+	//public final 생략해도 자동으로 붙음.
+	
+	void move(); // public abstract 생략되어 있음
 	void eat();
 	void sound();
 	void breathe();
---
+++ src/iducs/java/b/Lion.java
@@ -0,0 +1,17 @@
+package iducs.java.b;
+
+public class Lion extends Mammal{
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sound() {
+		// TODO Auto-generated method stub
+		System.out.println("æÓ»Ô");
+	}
+
+}
